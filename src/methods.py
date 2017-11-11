96a4ae518ce0bf3e0994da0d0d1f4dce271b4dc8
KiriteeGak <kiritee.gandamalla@gmail.com>
2017-11-11 12:15:04 +0530

    modified keys from str to tuples

--- src/methods.py
+++ src/methods.py
@@ -3,17 +3,16 @@ import datetime
 from scipy.misc import *
 
 
-class RegionGrowing:
+class RegionGrowing(object):
     def region_growing(self, image_array, file_path, cutoff_threshold, iterations):
         size = np.shape(image_array)
-        image_array_map = {str(r) + "_" + str(c): pixel for r, each_row in enumerate(image_array) for c, pixel in
+        image_array_map = {(r, c): pixel for r, each_row in enumerate(image_array) for c, pixel in
                            enumerate(each_row)}
-        seeds_map = {str(r) + "_" + str(c): 1 for (r, c) in self._seed_points(file_path)}
+        seeds_map = {(r, c): 1 for (r, c) in self._seed_points(file_path)}
         for it in range(0, iterations):
             _update_seeds_map = seeds_map.copy()
             for pixel_coord, strength in seeds_map.iteritems():
-                [r, c] = map(lambda a: int(a), pixel_coord.split('_'))
-                _temp_weights_neighbors_pixel = self._neighborhood_weighting([r, c], image_array_map,
+                _temp_weights_neighbors_pixel = self._neighborhood_weighting(pixel_coord, image_array_map,
                                                                              seeds_map, cutoff_threshold, size)
                 _update_seeds_map = self._update_weights(_temp_weights_neighbors_pixel, _update_seeds_map)
             seeds_map = _update_seeds_map
@@ -40,18 +39,18 @@ class RegionGrowing:
         _temp_weights = {}
         for i in range(-1, 2, 1):
             for j in range(-1, 2, 1):
-                pixel_key = str(r) + "_" + str(c)
+                pixel_key = (r, c)
                 if [i, j] != [0, 0] and 0 <= r + i < max_r and 0 <= c + j < max_c:
                     if pixel_key in seeds_map:
                         trans_strength = self._calculate_strength(image_map[pixel_key],
-                                                                  image_map[str(r + i) + "_" + str(c + j)],
+                                                                  image_map[(r + i, c + j)],
                                                                   seeds_map[pixel_key], threshold)
                     else:
                         trans_strength = self._calculate_strength(image_map[pixel_key],
-                                                                  image_map[str(r + i) + "_" + str(c + j)], 0,
+                                                                  image_map[(r + i, c + j)], 0,
                                                                   threshold)
                     if trans_strength != 0:
-                        _temp_weights[str(r + i) + "_" + str(c + j)] = trans_strength
+                        _temp_weights[(r + i, c + j)] = trans_strength
         return _temp_weights
 
     @staticmethod
@@ -67,7 +66,7 @@ class RegionGrowing:
         grown_image = np.zeros(size)
         for i in range(0, size[0]):
             for j in range(0, size[1]):
-                image_add = str(i) + "_" + str(j)
+                image_add = (i, j)
                 if image_add in image_map and image_map[image_add] > 0:
                     grown_image[i, j] = 255
                 else:
